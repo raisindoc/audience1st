fe5ef684814e291a9d005b708b92cf6d6dea00d6
Armando Fox <fox@cs.berkeley.edu>
2018-07-24 12:54:49 +0200

    started changing to generic button styles; test with blank URL in CSS stylesheet option

--- app/helpers/popup_help_helper.rb
+++ app/helpers/popup_help_helper.rb
@@ -2,7 +2,7 @@ module PopupHelpHelper
 
   def popup_help_for(item)
     unless (defined?(for_email)) || (m = t("popup_help.#{item}")).blank?
-      render :partial => 'messages/popup_help', :object => m
+      render :partial => 'shared/popup_help', :object => m
     end
   end
 
